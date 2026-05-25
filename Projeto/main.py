ca4f0a0f369707bf98137e72a0aef5b45b959368
miguelrdias1 <miguel.r.dias1@gmail.com>
2026-05-25 15:29:40 -0300

    fix: correção de meio de dump

--- Projeto/main.py
+++ Projeto/main.py
@@ -1,24 +1,52 @@
 import tkinter as tk
 from tkinter import messagebox
-from tkinter import simpledialog
 
 from prever import analisar
 
+def executar():
+
+    texto = entrada.get("1.0", tk.END)
+
+    resultado = analisar(texto)
+    if resultado==0:
+        resultado="Essa noticia é falsa!"
+    else:
+        resultado="Essa noticia é real"
+    messagebox.showinfo(
+        "Resultado",
+        f"Resultado: {resultado}"
+    )
+
 janela = tk.Tk()
 
-janela.withdraw()
+janela.title("Detector de Fake News")
 
-texto = simpledialog.askstring(
-    "Detector",
-    "Digite a notícia:"
+janela.geometry("700x400")
+
+titulo = tk.Label(
+    janela,
+    text="Digite a notícia:",
+    font=("Arial", 16)
 )
 
-resultado = analisar(texto)
-if resultado==0:
-    mensagem="Essa noticía é falsa!"
-else:
-    mensagem="Essa noticia é verdadeira"
-messagebox.showinfo(
-    "Resultado",
-    f"Resultado: {mensagem}"
+titulo.pack(pady=10)
+
+entrada = tk.Text(
+    janela,
+    width=70,
+    height=12,
+    font=("Arial", 12)
 )
+
+entrada.pack(pady=10)
+
+botao = tk.Button(
+    janela,
+    text="Analisar",
+    command=executar,
+    font=("Arial", 12)
+)
+
+botao.pack(pady=10)
+
+janela.mainloop()
--- Projeto/prever.py
+++ Projeto/prever.py
@@ -1,8 +1,14 @@
 import joblib
 import warnings
+import os
 warnings.filterwarnings("ignore")
-modelo = joblib.load("modelo.pkl")
-tfidf = joblib.load("tfidf.pkl")
+BASE_DIR = os.path.dirname(__file__)
+modelo_path = os.path.join(BASE_DIR, "modelo.pkl")
+tfidf_path = os.path.join(BASE_DIR, "tfidf.pkl")
+modelo = joblib.load(modelo_path)
+
+tfidf = joblib.load(tfidf_path)
+
 
 def analisar(noticia):
 
